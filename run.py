c9ecb75ecda74818b2e4a845d55d4ad26fc07325
Srirakshith Betageri <sbetageri111@gmail.com>
2019-10-12 18:18:03 -0500

    Complete data pipeline to train and evaluate test.

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import data
 import torch
 import pandas as pd
 
-from models import CrudeModel
+from models import AlexNet
 from torch.utils.data import DataLoader
 from sklearn.model_selection import train_test_split
 from Dataset import HistoDataset
@@ -13,9 +13,11 @@ def train_model(model, train_data, val_data, optimizer,
     for e in range(epochs):
         print('Training Iteration : ', e + 1)
         running_loss = 0
+        model = model.to(device)
         model.train()
         for img, label in tqdm(train_data):
             img = img.to(device)
+            label = label.view(label.size(0), 1)
             label = label.to(device)
             optimizer.zero_grad()
             output = model(img)
@@ -32,6 +34,7 @@ def train_model(model, train_data, val_data, optimizer,
             running_loss = 0
             for img, label in tqdm(val_data):
                 img = img.to(device)
+                label = label.view(label.size(0), 1)
                 label = label.to(device)
                 output = model(img)
                 loss = loss_fn(output, label)
@@ -39,13 +42,16 @@ def train_model(model, train_data, val_data, optimizer,
         print('Avg Loss : ', running_loss / (len(val_data) * val_data.batch_size))
     return model
 
-def predict(model, test_data):
+def predict(model, test_data, device):
     outputs = []
     print('Predicting')
     with torch.no_grad():
         model.eval()
-        for img in tqmd(test_data):
+        for img in tqdm(test_data):
+            img = img.view(1, *img.size())
+            img = img.to(device)
             output = model(img)
+            output = torch.sigmoid(output)
             outputs.append(output.item())
     return outputs
 
@@ -62,13 +68,19 @@ if __name__ == '__main__':
     train_loader = DataLoader(train_dataset, batch_size=4)
     val_loader = DataLoader(val_dataset, batch_size=4)
 
-    model = CrudeModel.CrudeModel()
+    model = AlexNet.AlexNet()
 
-    model = train_model()
+    optimizer = torch.optim.Adadelta(model.parameters())
+    loss_fn = torch.nn.BCEWithLogitsLoss()
 
-    model.save(model.state_dict(), 'models/model.pt')
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    predictions = predict(model, test_dataset)
+    model = train_model(model, train_loader, val_loader, optimizer, 20, loss_fn, device)
+
+    torch.save(model.state_dict(), 'models/model.pt')
+
+    predictions = predict(model, test_dataset, device)
 
     test_df['label'] = predictions
 
+    test_df.to_csv('predictions.csv', index=False)
