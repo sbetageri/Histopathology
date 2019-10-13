202bc633523fc2c4cfc4f8cdba2859ca32184f87
Srirakshith Betageri <sbetageri111@gmail.com>
2019-10-13 12:30:57 -0500

    Use half of AlexNet.

--- models/AlexNet.py
+++ models/AlexNet.py
@@ -5,16 +5,17 @@ import torch.nn.functional as F
 class AlexNet(nn.Module):
     def __init__(self):
         super(AlexNet, self).__init__()
-        self.conv1 = nn.Conv2d(3, 12, 5)
-        self.conv2 = nn.Conv2d(12, 32, 3)
-        self.conv3 = nn.Conv2d(32, 48, 3)
-        self.conv4 = nn.Conv2d(48, 48, 3)
-        self.conv5 = nn.Conv2d(48, 32, 3)
+        self.conv1 = nn.Conv2d(3, 48, 5)
+        self.conv2 = nn.Conv2d(48, 128, 3)
+        self.conv3 = nn.Conv2d(128, 192, 3)
+        self.conv4 = nn.Conv2d(192, 192, 3)
+        self.conv5 = nn.Conv2d(192, 128, 3)
 
-        self.linear1 = nn.Linear(2048, 256)
-        self.linear2 = nn.Linear(256, 1)
+        self.linear1 = nn.Linear(8192, 64)
+        self.linear2 = nn.Linear(64, 1)
 
         self.max_pool = nn.MaxPool2d(2)
+        self.dropout = nn.Dropout(0.45)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
@@ -29,6 +30,8 @@ class AlexNet(nn.Module):
         x = x.view((x.size(0), -1))
 
         x = F.relu(self.linear1(x))
+        x = self.dropout(x)
         x = self.linear2(x)
+
         return x
 
