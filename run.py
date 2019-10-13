5d73cf75574ad1cef58240645dc32c49830608c5
Srirakshith Betageri <sbetageri111@gmail.com>
2019-10-12 23:57:10 -0500

    Track accuracy scores.

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ import pandas as pd
 from models import AlexNet
 from torch.utils.data import DataLoader
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 from Dataset import HistoDataset
 from tqdm import tqdm
 
@@ -13,6 +14,7 @@ def train_model(model, train_data, val_data, optimizer,
     for e in range(epochs):
         print('Training Iteration : ', e + 1)
         running_loss = 0
+        running_acc = 0
         model = model.to(device)
         model.train()
         for img, label in tqdm(train_data):
@@ -22,11 +24,14 @@ def train_model(model, train_data, val_data, optimizer,
             optimizer.zero_grad()
             output = model(img)
             loss = loss_fn(output, label)
-            running_loss += loss.item()
             loss.backward()
             optimizer.step()
+            running_loss += loss.item()
+            predictions = torch.ceil(output.detach())
+            running_acc += accuracy_score(label, predictions)
 
         print('Avg Loss : ', running_loss / (len(train_data) * train_data.batch_size))
+        print('Avg Acc: ', running_acc / (len(train_data) * train_data.batch_size))
 
         with torch.no_grad():
             print('Validation Iteration : ', e + 1)
@@ -38,8 +43,11 @@ def train_model(model, train_data, val_data, optimizer,
                 label = label.to(device)
                 output = model(img)
                 loss = loss_fn(output, label)
+                predictions = torch.ceil(output.detach())
+                running_acc += accuracy_score(label, predictions)
                 running_loss += loss.item()
         print('Avg Loss : ', running_loss / (len(val_data) * val_data.batch_size))
+        print('Avg Acc: ', running_acc / (len(train_data) * train_data.batch_size))
     return model
 
 def predict(model, test_data, device):
@@ -75,7 +83,7 @@ if __name__ == '__main__':
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    model = train_model(model, train_loader, val_loader, optimizer, 20, loss_fn, device)
+    model = train_model(model, train_loader, val_loader, optimizer, 2, loss_fn, device)
 
     torch.save(model.state_dict(), 'models/model.pt')
 
